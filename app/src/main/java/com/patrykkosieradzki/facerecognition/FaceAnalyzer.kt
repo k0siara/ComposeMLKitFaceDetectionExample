31ffe84f254608bb333b3842db6487efb98f96df
Patryk Marciszek-Kosieradzki <patryk.marciszek-kosieradzki@payeye.com>
2021-08-25 23:54:00 +0200

    Initial commit

---
+++ app/src/main/java/com/patrykkosieradzki/facerecognition/FaceAnalyzer.kt
@@ -0,0 +1,42 @@
+package com.patrykkosieradzki.facerecognition
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+
+class FaceAnalyzer : ImageAnalysis.Analyzer {
+
+    private val realTimeOpts = FaceDetectorOptions.Builder()
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+        .setMinFaceSize(0.20f)
+        .enableTracking()
+        .build()
+
+    private val detector = FaceDetection.getClient(realTimeOpts)
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        mediaImage?.let {
+            val inputImage =
+                InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            detector.process(inputImage)
+                .addOnSuccessListener { faces ->
+                    imageProxy.close()
+                }
+                .addOnFailureListener {
+                    imageProxy.close()
+                }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/patrykkosieradzki/facerecognition/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.patrykkosieradzki.facerecognition
+
+import android.os.Bundle
+import android.util.Size
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
+import com.patrykkosieradzki.facerecognition.ui.theme.MLKitFaceRecognitionExampleTheme
+
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MLKitFaceRecognitionExampleTheme {
+                FaceRecognitionScreen()
+            }
+        }
+    }
+}
+
+@ExperimentalPermissionsApi
+@Composable
+fun FaceRecognitionScreen() {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    PermissionRequired(
+        permissionState = cameraPermissionState,
+        permissionNotGrantedContent = {
+            LaunchedEffect(Unit) {
+                cameraPermissionState.launchPermissionRequest()
+            }
+        },
+        permissionNotAvailableContent = {
+            Column {
+                Text(
+                    "Camera permission denied."
+                )
+            }
+        }
+    ) {
+        FaceRecognitionScreenContent()
+    }
+}
+
+@Composable
+fun FaceRecognitionScreenContent() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+    Scaffold {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AndroidView(
+                modifier = Modifier.matchParentSize(),
+                factory = { ctx ->
+                    val previewView = PreviewView(ctx)
+                    val executor = ContextCompat.getMainExecutor(ctx)
+
+                    cameraProviderFuture.addListener({
+                        val cameraProvider = cameraProviderFuture.get()
+                        val preview = Preview.Builder().build().also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
+
+                        val cameraSelector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+                            .build()
+
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(Size(previewView.width, previewView.height))
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .setImageQueueDepth(10)
+                            .build()
+                            .apply {
+                                setAnalyzer(executor, FaceAnalyzer())
+                            }
+
+                        cameraProvider.unbindAll()
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            cameraSelector,
+                            preview,
+                            imageAnalysis
+                        )
+                    }, executor)
+                    previewView
+                },
+            )
+
+//            Canvas(
+//                modifier = Modifier.matchParentSize()
+//            ) {
+//
+//            }
+        }
+    }
+}
